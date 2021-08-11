4ced104347080cb5fe6074c002ab9c3058aedbf0
cspyc <piyicheng001@qq.com>
2021-08-11 22:24:48 +0800

    build the scaffold of project

---
+++ src/main/java/me/pyc/project/init/ProjectFirst.java
@@ -0,0 +1,9 @@
+package me.pyc.project.init;
+
+/**
+ * @author pi
+ * @date 2021/08/11 22:20:33
+ **/
+public class ProjectFirst {
+
+}
---
+++ src/test/java/me/pyc/project/init/ProjectFirstTest.java
@@ -0,0 +1,16 @@
+package me.pyc.project.init;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author pi
+ * @date 2021/08/11 22:19:03
+ **/
+public class ProjectFirstTest {
+    @Test
+    @DisplayName("第一个Junit5测试")
+    public void should_initial_first_junit5_test() throws Exception {
+        ProjectFirst projectFirst = new ProjectFirst();
+    }
+}
